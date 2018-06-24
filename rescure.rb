b124b26e8945a8e0cccb65f4ba79190f56d48df9
shkobm <scrt.8shbabe@gmail.com>
2018-06-25 00:35:10 +0900

    Cretae regexp_checker

--- rescure.rb
+++ rescure.rb
@@ -1,38 +1,17 @@
-def method_1
-  puts 'method_1 start.'
+def fizz_buzz(n)
   begin
-    method_2
-  rescue
-    puts '例外が発生しました'
-  end
-  puts 'method_1 end.'
-end
-
-def method_2
-  puts 'method_2 start.'
-  method_3
-  puts 'method_2 end.'
-end
-
-def method_3
-  puts 'method_3 start.'
-    @culc = 2 / 0
-  puts 'method_3 end.'
-end
-
-method_1
-retry_count = 0
-
-begin
-  1 / 0
-rescue NoMethodError
-  puts 'NoMethodErrorです'
-rescue NameError
-  puts 'NameErrorです'
-rescue
-  retry_count += 1
-  if retry_count <= 3
-  puts "その他のエラーです。retryします（#{retry_count}回目です）"
-    retry
+    if n % 15 == 0
+      'Fizz Buzz'
+    elsif n % 5 == 0
+      'Fizz'
+    elsif n % 3 == 0
+      'Buzz'
+    else
+      n.to_s
+    end
+  rescue => e
+    puts "#{e.message} #{e.class}"
   end
 end
+
+fizz_buzz(0)
